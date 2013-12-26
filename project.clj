fdd030a7c016ccf1b13b5aca966463260db77a4d
Andrew Bruce <andrew@camelpunch.com>
2013-12-25 21:28:26 -0800

    Install expectations

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [
+            [lein-expectations "0.0.7"]
+            [lein-autoexpect "1.2.1"]
+            ]
+  :profiles {:dev {:dependencies [[expectations "1.4.52"]]}})
--- test/cu/core_test.clj
+++ test/cu/core_test.clj
@@ -1,7 +1,5 @@
 (ns cu.core-test
-  (:require [clojure.test :refer :all]
+  (:require [expectations :refer :all]
             [cu.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(expect 3 3)
